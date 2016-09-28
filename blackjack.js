8950e38743b8707c1e3231424faa91e317de51fe
Andrew Toups <mistertoups@gmail.com>
2016-09-28 19:06:43 -0400

    refactored code and added ace handling

--- blackjack.js
+++ blackjack.js
@@ -1,7 +1,21 @@
 (function runGame() {
     // debugger;
+    function initialize() {
+      display.innerHTML = '';
+      getCard(2); // deal hand
+    }
+
+    document.getElementById('stand').addEventListener('click', function() {
+      checkResult(true);
+    });
+
+    document.getElementById('hit').addEventListener('click', function() {
+      getCard(1);
+    });
+
     var display = document.getElementById('cards');
     var cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    initialize();
 
     function getCard(numOfCards) {
         for (i = 0; i < numOfCards; i++) {
@@ -17,6 +31,47 @@
         }, 100);
     }
 
+    function tabulate() {
+      var playerHand = display.innerHTML.split(' ');
+      var playerTotal = 0;
+      for (var index =0; index < playerHand.length; index++) {
+        var currentCard = playerHand[index];
+        if (Number(currentCard)) {
+          currentCard = Number(currentCard);
+          playerTotal += currentCard;
+        }
+
+        if (currentCard === 'J' || currentCard === 'Q' || currentCard === 'K') {
+          playerTotal += 10;
+        }
+
+        if (currentCard === 'A') { //fuck my life but this is another loop that finishes
+          index++;                ///adding the array. any future aces are counted as one
+          for (index; index < playerHand.length; index++){ //because even a hand of two aces
+            currentCard = playerHand[index];               //would go over 21 if both were
+            if (Number(currentCard)) {                     //valued as 11
+              currentCard = Number(currentCard);           //is there a way to do this
+              playerTotal += currentCard;                 ///recursively??
+            }
+
+            if (currentCard === 'J' || currentCard === 'Q' || currentCard === 'K') {
+              playerTotal += 10;
+            }
+
+            if (currentCard == 'A'){
+              playerTotal += 1;
+            }
+          }
+          if (playerTotal + 11 > 21){
+            playerTotal += 1;
+          } else {
+          playerTotal += 11;
+          }
+        }
+    } // end of check hand total
+
+      return playerTotal;
+    }
 
     /**
      * Check the result of the current cards and alert the game result
@@ -26,56 +81,38 @@
      * @return {void}
      */
     function checkResult(standing) {
-        var yourHand = display.innerHTML.split(' ');
-        var playerTotal = 0;
-        yourHand.forEach(function(currentCard, i) {   // check hand total TODO: make function
-            if (Number(currentCard)) {
-                currentCard = Number(currentCard);
-                playerTotal += currentCard;
-            }
+        var playerTotal = tabulate();
+        var diff;
+        var gameOver = false;
 
-            if (currentCard === 'J' || currentCard == 'Q' || currentCard === 'K') {
-                playerTotal += 10;
-            }
-
-            if (currentCard === 'A') {
-                playerTotal += 11;
-            }
-        }); // end of check hand total
-        console.log("player total = " + playerTotal);
         if (playerTotal > 21) { // player goes over
-          alert('Bust!');
+          diff = playerTotal - 21;
+          alert('Bust! ' + diff + " over!");
+          gameOver = true; // game ends
         }
 
-        if (playerTotal === 21) {
+        if (playerTotal === 21) { //blackjack
           alert('Blackjack! You win!');
+          gameOver = true; // game ends
         }
 
-        if (standing) {
+        if (standing) { //compare player hand to dealer hand on standing
           if (playerTotal < 15) {
-            alert('Dealer wins.');
+            diff = 15 - playerTotal;
+            alert('Dealer wins. ' + diff + ' under the dealer.');
           } else if (playerTotal < 19) {
             alert('Push!');
           } else {
-            alert('Player wins!');
+            diff = playerTotal - 18;
+            alert('Player wins! ' + diff + " over Dealer's hand");
           }
+          gameOver = true; // game ends
         }
 
+        if (gameOver){
+          initialize();
+        }
 
-
-        // display.innerHTML = '';
-        // card = Math.floor(Math.random() * cards.length);
-        // display.innerHTML = cards[card];
     }
 
-    document.getElementById('stand').addEventListener('click', function() {
-        checkResult(true);
-    });
-
-    document.getElementById('hit').addEventListener('click', function() {
-        getCard(1);
-    });
-
-    display.innerHTML = '';
-    getCard(2); // deal hand
 })();
